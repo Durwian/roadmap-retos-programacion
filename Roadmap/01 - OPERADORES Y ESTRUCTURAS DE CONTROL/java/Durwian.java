48ca4b97d0f6c13d982a9a65c8d976bfc3442410
Montse Forner <vicforner@gmail.com>
2024-06-14 14:43:01 +0200

    01 - Java

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/Durwian.java
@@ -0,0 +1,64 @@
+class Durwian{
+
+
+    //Operadores
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        if (condition) {
+            
+        }
+        
+        if (condition) {
+            
+        } else {
+            
+        }
+
+        String[] strings ={"Hola", "que tal", "como estas?"};
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+
+        while (condition) {
+            
+        }
+
+        do {
+            
+        } while (condition);
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        try () {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }finally{
+
+        }
+
+
+        for (int i = 10; i < 50; i++) {
+            if(i%2 == 0 && i != 16){
+                if(i%3 > 0){
+                    System.out.print(i + ", ");
+                }
+            }
+        }
+        System.out.println();
+
+
+    }
+}
